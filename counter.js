434d38c6d405e0fb9d2fcd5c05f88282e13cf75d
Jeroen <Jeroenvinke17@gmail.com>
2016-08-09 11:09:48 +0200

    first commit

---
+++ counter.js
@@ -0,0 +1,24 @@
+define(['exports', 'aurelia-templating'], function (exports, _aureliaTemplating) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.Counter = undefined;
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var _dec, _dec2, _class;
+
+  var Counter = exports.Counter = (_dec = (0, _aureliaTemplating.customElement)('counter'), _dec(_class = function () {
+    function Counter() {
+      _classCallCheck(this, Counter);
+    }
+
+    return Counter;
+  }()) || _class);
+});
